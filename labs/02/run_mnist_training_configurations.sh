62e5740deb7bae7554d61025ace83d26eec626ea
Karel Ha <mathemage@gmail.com>
2018-03-13 12:09:16 +0100

    Add the rest of args to run_mnist_trainining
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/02/run_mnist_training_configurations.sh
+++ labs/02/run_mnist_training_configurations.sh
@@ -10,10 +10,15 @@ ARGS=(
 "Adam 0.001"
 "Adam 0.01 --learning_rate_final 0.001"
 )
+#95.97    mnist_training.py --optimizer=SGD --learning_rate=0.01
+#97.99    mnist_training.py --optimizer=SGD --learning_rate=0.01 --momentum=0.9
+#98.01    mnist_training.py --optimizer=SGD --learning_rate=0.1
+#97.98    mnist_training.py --optimizer=Adam --learning_rate=0.001
+#98.15    mnist_training.py --optimizer=Adam --learning_rate=0.01 --learning_rate_final=0.001
 
 for line in "${ARGS[@]}"; do
     arg=($line)
-    command="$INTERPRETER $SCRIPT --optimizer ${arg[0]} --learning_rate ${arg[1]}"
+    command="$INTERPRETER $SCRIPT --optimizer ${arg[0]} --learning_rate ${arg[1]} ${arg[2]} ${arg[3]}"
     echo ${command}
     ${command}
     echo
