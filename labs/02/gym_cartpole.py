ab62b73b878fa787d3b0c7294253e697b895c33f
Karel Ha <mathemage@gmail.com>
2018-03-18 21:23:31 +0100

    Design NN for 04gym_cartpole
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/02/gym_cartpole.py
+++ labs/02/gym_cartpole.py
@@ -5,100 +5,110 @@ import tensorflow as tf
 # pip3 install --user gym==0.9.5
 # small dataset <- do not overfit: small capacity, simple model, regularization
 
+# noinspection PyAttributeOutsideInit
 class Network:
-    OBSERVATIONS = 4
-    ACTIONS = 2
+		OBSERVATIONS = 4
+		ACTIONS = 2
 
-    def __init__(self, threads, seed=42):
-        # Create an empty graph and a session
-        graph = tf.Graph()
-        graph.seed = seed
-        self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
-                                                                       intra_op_parallelism_threads=threads))
+		def __init__(self, threads, seed=42):
+				# Create an empty graph and a session
+				graph = tf.Graph()
+				graph.seed = seed
+				self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+																																			 intra_op_parallelism_threads=threads))
 
-    def construct(self, args):
-        with self.session.graph.as_default():
-            self.observations = tf.placeholder(tf.float32, [None, self.OBSERVATIONS], name="observations")
-            self.labels = tf.placeholder(tf.int64, [None], name="labels")
+		def construct(self, args):
+				with self.session.graph.as_default():
+						self.observations = tf.placeholder(tf.float32, [None, self.OBSERVATIONS], name="observations")
+						self.labels = tf.placeholder(tf.int64, [None], name="labels")
 
-            # TODO: Define the model, with the output layers for actions in `output_layer`
-            # TODO: number of hidden layers (as low as possible)
-            # TODO: size of hidden layers
-            # TODO: activation functions
-            # TODO: optimizer
-            # TODO: (optionally) batch size
+						# TODO: Define the model, with the output layers for actions in `output_layer`
+						depth_of_hidden_layers = 2        # TODO: number of hidden layers (as low as possible)
+						width_of_hidden_layer = 2         # TODO: size of hidden layers
+						activation_function = tf.nn.relu  # TODO: activation functions
 
-            self.actions = tf.argmax(output_layer, axis=1, name="actions")
+						input_layer = tf.layers.Input(tensor=self.observations)
+						hidden_layers = [None] * (1 + depth_of_hidden_layers)
+						hidden_layers[0] = input_layer
+						for depth in range(1, 1 + depth_of_hidden_layers):
+							hidden_layers[depth] = tf.layers.dense(hidden_layers[depth - 1], width_of_hidden_layer,
+							                                       activation=activation_function, name="hidden_layer{}".format(depth))
+						last_hidden_layer = hidden_layers[-1]
+						output_layer = tf.layers.dense(last_hidden_layer, self.ACTIONS, activation=None, name="output_layer")
+						self.actions = tf.argmax(output_layer, axis=1, name="actions")
 
-            # Global step
-            loss = tf.losses.sparse_softmax_cross_entropy(self.labels, output_layer, scope="loss")
-            global_step = tf.train.create_global_step()
-            self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name="training")
+						# Global step
+						loss = tf.losses.sparse_softmax_cross_entropy(self.labels, output_layer, scope="loss")
+						global_step = tf.train.create_global_step()
+						self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name="training")
 
-            # Summaries
-            accuracy = tf.reduce_mean(tf.cast(tf.equal(self.labels, self.actions), tf.float32))
-            summary_writer = tf.contrib.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
-            with summary_writer.as_default(), tf.contrib.summary.record_summaries_every_n_global_steps(100):
-                self.summaries = [tf.contrib.summary.scalar("train/loss", loss),
-                                  tf.contrib.summary.scalar("train/accuracy", accuracy)]
+						# Summaries
+						accuracy = tf.reduce_mean(tf.cast(tf.equal(self.labels, self.actions), tf.float32))
+						summary_writer = tf.contrib.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
+						with summary_writer.as_default(), tf.contrib.summary.record_summaries_every_n_global_steps(100):
+								self.summaries = [tf.contrib.summary.scalar("train/loss", loss),
+																	tf.contrib.summary.scalar("train/accuracy", accuracy)]
 
-            # collections = list of nodes in comp graph - saver
-            # Construct the saver
-            tf.add_to_collection("end_points/observations", self.observations)
-            tf.add_to_collection("end_points/actions", self.actions)
-            self.saver = tf.train.Saver()
+						# collections = list of nodes in comp graph - saver
+						# Construct the saver
+						tf.add_to_collection("end_points/observations", self.observations)
+						tf.add_to_collection("end_points/actions", self.actions)
+						self.saver = tf.train.Saver()
 
-            # Initialize variables
-            self.session.run(tf.global_variables_initializer())
-            with summary_writer.as_default():
-                tf.contrib.summary.initialize(session=self.session, graph=self.session.graph)
+						# Initialize variables
+						self.session.run(tf.global_variables_initializer())
+						with summary_writer.as_default():
+								tf.contrib.summary.initialize(session=self.session, graph=self.session.graph)
 
-    def train(self, observations, labels):
-        self.session.run([self.training, self.summaries], {self.observations: observations, self.labels: labels})
+		def train(self, observations, labels):
+				self.session.run([self.training, self.summaries], {self.observations: observations, self.labels: labels})
 
-    def save(self, path):
-        self.saver.save(self.session, path)
+		def save(self, path):
+				self.saver.save(self.session, path)
 
 
 if __name__ == "__main__":
-    import argparse
-    import datetime
-    import os
-    import re
+		import argparse
+		import datetime
+		import os
+		import re
 
-    # Fix random seed
-    np.random.seed(42)
+		# Fix random seed
+		np.random.seed(42)
 
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
-    parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-    args = parser.parse_args()
+		# Parse arguments
+		parser = argparse.ArgumentParser()
+		# parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
+		parser.add_argument("--epochs", default=10000, type=int, help="Number of epochs.")
+		parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+		# TODO: batch size (optional)
+		args = parser.parse_args()
 
-    # Create logdir name
-    args.logdir = "logs/{}-{}-{}".format(
-        os.path.basename(__file__),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
-    )
-    if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
+		# Create logdir name
+		args.logdir = "logs/{}-{}-{}".format(
+				os.path.basename(__file__),
+				datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+				",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
+		)
+		if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
 
-    # Load the data
-    observations, labels = [], []
-    with open("gym_cartpole-data.txt", "r") as data:
-        for line in data:
-            columns = line.rstrip("\n").split()
-            observations.append([float(column) for column in columns[0:4]])
-            labels.append(int(columns[4]))
-    observations, labels = np.array(observations), np.array(labels)
+		# Load the data
+		observations, labels = [], []
+		with open("gym_cartpole-data.txt", "r") as data:
+				for line in data:
+						columns = line.rstrip("\n").split()
+						observations.append([float(column) for column in columns[0:4]])
+						labels.append(int(columns[4]))
+		observations, labels = np.array(observations), np.array(labels)
 
-    # Construct the network
-    network = Network(threads=args.threads)
-    network.construct(args)
+		# Construct the network
+		network = Network(threads=args.threads)
+		network.construct(args)
 
-    # Train
-    for i in range(args.epochs):
-        # TODO: Train for an epoch
+		# Train
+		for i in range(args.epochs):
+			# TODO: Train for an epoch
+			network.train(observations, labels)
 
-    # Save the network
-    network.save("gym_cartpole/model")
+		# Save the network
+		network.save("gym_cartpole/model")
