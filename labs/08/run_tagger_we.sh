0ebcf4bc078d650a2c816b53fba249bee7add0f9
Karel Ha <mathemage@gmail.com>
2018-09-29 11:56:54 +0200

    Remove `--rnn_cell=RNN` from `run_tagger_we.sh`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/08/run_tagger_we.sh
+++ labs/08/run_tagger_we.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-python3 ./tagger_we_sol.py --rnn_cell=RNN --rnn_cell_dim=5
-python3 ./tagger_we_sol.py --rnn_cell=RNN --rnn_cell_dim=10
-python3 ./tagger_we_sol.py --rnn_cell=RNN --rnn_cell_dim=50
-
 python3 ./tagger_we_sol.py --rnn_cell=GRU --rnn_cell_dim=5
 python3 ./tagger_we_sol.py --rnn_cell=GRU --rnn_cell_dim=10
 python3 ./tagger_we_sol.py --rnn_cell=GRU --rnn_cell_dim=50
